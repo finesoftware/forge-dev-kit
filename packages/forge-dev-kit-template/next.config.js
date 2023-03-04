06847f51fd5f59e3d491d1679abfa1da093fa26e
Hannes Obweger <hobweger@gmail.com>
2023-03-04 19:20:28 +0100

    Fix dev server websocket issues

--- packages/forge-dev-kit-template/next.config.js
+++ packages/forge-dev-kit-template/next.config.js
@@ -2,20 +2,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const withImages = require('next-images');
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = withImages({
-    images: {
-        disableStaticImages: true,
-    },
-    experimental: {
-        externalDir: true,
-    },
-    pageExtensions: ['page.tsx'],
-    webpack: (config) => {
-        config.resolve.mainFields = ['browser', 'module', 'main'];
-        return config;
-    },
-    assetPrefix: '.',
-    // required for forge tunneling
-    compress: false,
-});
+module.exports = (phase) =>
+    withImages({
+        images: {
+            disableStaticImages: true,
+        },
+        experimental: {
+            externalDir: true,
+        },
+        pageExtensions: ['page.tsx'],
+        webpack: (config) => {
+            config.resolve.mainFields = ['browser', 'module', 'main'];
+            return config;
+        },
+        // without the phase check we're running into websocket issues with fast reload;
+        // see https://stackoverflow.com/a/73879833
+        assetPrefix: phase === PHASE_DEVELOPMENT_SERVER ? undefined : '.',
+        // required for forge tunneling
+        compress: false,
+    });
